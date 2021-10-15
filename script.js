5645c1588617f598863d2040a76c571ab5c8fc4e
MickTK <63038410+MickTK@users.noreply.github.com>
2021-10-15 15:17:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,7 @@
-/* Get URL Parameters */
 const urlParams = new URLSearchParams(window.location.search);
 var json;
 
 $(document).ready(function(){
-
-    /* 
-        Parameters usage
-        Query example = ?test1=foo&test2=foo
-    */
     $("#test1").text(urlParams.get('test1'));
     $("#test2").text(urlParams.get('test2'));
 
@@ -28,5 +22,5 @@ function request(json){
            alert('Something went wrong!');
            return undefined;
         }
-      });
+    });
 }
