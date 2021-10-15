c2bc8fd4c6c4f6d4e68dbbc1af4d83da4ce6a297
MickTK <63038410+MickTK@users.noreply.github.com>
2021-10-15 15:53:47 +0200

    Update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 var json;
 
 $(document).ready(function(){
-    $("#test1").text(getParam('test1'));
-    $("#test2").text(getParam('test2'));
+    $("#test1").text("test1 = " + getParam('test1'));
+    $("#test2").text("test2 = " + getParam('test2'));
 
     request(json, 'https://micktk.github.io/JS-Server/Database/db.json');
 });
