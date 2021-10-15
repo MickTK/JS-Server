85903639d92771d18b9d0d479f8d84c91ca8e8d2
MickTK <63038410+MickTK@users.noreply.github.com>
2021-10-15 13:24:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+/* Get URL Parameters */
 const urlParams = new URLSearchParams(window.location.search);
 
 $(document).ready(function(){
+
+    /* 
+        Parameters usage
+        Query example = ?test1=foo&test2=foo
+    */
     $("#test1").text(urlParams.get('test1'));
     $("#test2").text(urlParams.get('test2'));
+
 });
