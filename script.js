39e7766d86f42c615e35a27bf3c8c3f42319aae7
MickTK <63038410+MickTK@users.noreply.github.com>
2021-10-15 15:29:09 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,31 @@
-const urlParams = new URLSearchParams(window.location.search);
 var json;
 
 $(document).ready(function(){
-    $("#test1").text(urlParams.get('test1'));
-    $("#test2").text(urlParams.get('test2'));
+    $("#test1").text(getParam('test1'));
+    $("#test2").text(getParam('test2'));
 
-    json = request('https://micktk.github.io/JS-Server/Database/db.json');
+    request(json, 'https://micktk.github.io/JS-Server/Database/db.json');
 });
 
-function request(json){
+function getParam(param){
+    return new URLSearchParams(window.location.search).get(param);
+}
+
+function request(json, url){
+    var result;
     $.ajax({
         type: 'GET',
         dataType: 'json',
-        url: json,
+        url: url,
         data: {},
         success: function(data){
            alert(data.foo);
-           return data;
+           json = data;
+           console.log(json);
         },
         error: function(){
            alert('Something went wrong!');
-           return undefined;
+           json = undefined;
         }
     });
 }
